a2821b3c8aeb06a02dfb8b66f6dcb0271aa7e259
melpon <shigemasa7watanabe+github@gmail.com>
2018-01-23 10:58:07 +0900

    bump version to 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mutation.Mixfile do
   def project do
     [
       app: :mutable,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
